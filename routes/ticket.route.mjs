ed2d2152fea7320786ac1b2d1296777ac033fd75
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-10-21 16:16:45 +0530

    added fetch closed tickets

--- routes/ticket.route.mjs
+++ routes/ticket.route.mjs
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
 import { body, validationResult } from 'express-validator';
-import { getAllTicketsOpenTicket } from "../service/ticketInfo.service.mjs";
+import { getAllTicketsClosedTicket, getAllTicketsOpenTicket } from "../service/ticketInfo.service.mjs";
 import { getBusById } from "../model/ticketInfo.model.mjs";
 
 const ticketRouter = Router();
 ticketRouter.use('/ticket', bodyParser.json());
 
-async function allOpenTickets(req, res) {
+async function getOpenTickects(req, res) {
     try {
         const tickets = await getAllTicketsOpenTicket();
         res.send(tickets);
@@ -16,9 +16,20 @@ async function allOpenTickets(req, res) {
     }
 }
 ticketRouter.get('/ticket/all/open',
-    allOpenTickets
+    getOpenTickects
 );
 
+async function getClosedTickets(req, res) {
+    try {
+        const tickets = await getAllTicketsClosedTicket();
+        res.send(tickets);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+}
+ticketRouter.get('/ticket/all/close',
+    getClosedTickets
+);
 
 ticketRouter.get('/bus',
     async (req, res) => {
--- service/ticketInfo.service.mjs
+++ service/ticketInfo.service.mjs
@@ -11,7 +11,7 @@ async function getAllTicketsOpenTicket() {
 
 async function getAllTicketsClosedTicket(phoneNumber) {
     try {
-        return await queryUserByPhoneNumber(false);
+        return await getAllTicketsByState(false);
     } catch (err) {
         console.error(err, 'Failed to load ticket state from persistent storage');
         throw new Error('Failed to load ticket state from persistent storage');
